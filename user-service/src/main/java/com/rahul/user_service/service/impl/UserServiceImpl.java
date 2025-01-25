d90232a305778f8f8be9245e54742df7370e89db
RAHUL <rahulmulik9@gmail.com>
2025-01-25 13:26:35 +0530

    Added implementation of UserServiceImpl Added exception

---
+++ user-service/src/main/java/com/rahul/user_service/service/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.rahul.user_service.service.impl;
+
+
+import com.rahul.user_service.repository.UserRepository;
+import com.rahul.user_service.entity.User;
+import com.rahul.user_service.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        //implement RATING SERVICE CALL: USING REST TEMPLATE
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUser(String userId) {
+        return userRepository.findById(userId).orElseThrow();
+    }
+
+
+}
