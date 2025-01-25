f693cbd427dd407a826ed6f75ac76515f01a8cdc
RAHUL <rahulmulik9@gmail.com>
2025-01-25 13:18:05 +0530

    Changes application properties Added mysql connection added entity classes

---
+++ user-service/src/main/java/com/rahul/user_service/entity/User.java
@@ -0,0 +1,33 @@
+package com.rahul.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "micro_users")
+public class User {
+
+    @Id
+    @Column(name = "ID")
+    private String userId;
+
+    @Column(name = "NAME", length = 20)
+    private String name;
+
+    @Column(name = "EMAIL")
+
+    private String email;
+    @Column(name = "ABOUT")
+    private String about;
+    //other user properties that you want
+
+    @Transient
+    private List<Rating> ratings=new ArrayList<>();
+
+
+}
