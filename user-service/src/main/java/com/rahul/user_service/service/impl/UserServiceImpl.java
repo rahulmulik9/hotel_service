0232e173e182c685afde924c5cc96dd82ec1e773
RAHUL <rahulmulik9@gmail.com>
2025-01-26 20:12:33 +0530

    Application name and database name renamed to same manner

--- user-service/src/main/java/com/rahul/user_service/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/rahul/user_service/service/impl/UserServiceImpl.java
@@ -48,21 +48,20 @@ public class UserServiceImpl implements UserService {
 
         User theUser = userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User not found"));
         //get rating from user directly (require the bean of  Rest template object which is declared in the main class)
-        //ArrayList ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/e1f229aa-abf5-47d6-9aba-c6ee012473b9", ArrayList.class);
-        //Rating[] ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+theUser.getUserId(), Rating[].class);
-
-        //also fetching hotels data
-        Rating[] ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + theUser.getUserId(), Rating[].class);
-
+        /*Rating[] ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/e1f229aa-abf5-47d6-9aba-c6ee012473b9", Rating[].class);
         List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
         List<Rating> ratingList = ratings.stream().map(rating -> {
-            //api call to hotel service to get the hotel
             Hotel hotel = restTemplate.getForObject("http://localhost:8082/hotels/1b6b9408-1221-4bf6-9951-8faaa81a0f6a", Hotel.class);
             //Hotel hotel = hotelService.getHotel(rating.getHotelId());
-            // logger.info("response status code: {} ",forEntity.getStatusCode());
-            //set the hotel to rating
             rating.setHotel(hotel);
-            //return the rating
+            return rating;
+        }).collect(Collectors.toList());*/
+
+        //also fetching hotels data
+        Rating[] ratingsOfUser = restTemplate.getForObject("http://RATING_SERVICE/ratings/users/" + theUser.getUserId(), Rating[].class);List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
+        List<Rating> ratingList = ratings.stream().map(rating -> {
+            Hotel hotel = restTemplate.getForObject("http://HOTEL_SERVICE/hotels/"+rating.getHotelId(), Hotel.class);
+            rating.setHotel(hotel);
             return rating;
         }).collect(Collectors.toList());
 
