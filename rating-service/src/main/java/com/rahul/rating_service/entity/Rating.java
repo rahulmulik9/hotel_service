efd8624328b5da7c71464392b297bf19e60ccd8e
RAHUL <rahulmulik9@gmail.com>
2025-01-26 17:03:38 +0530

    Code factor

--- rating-service/src/main/java/com/rahul/rating_service/entity/Rating.java
+++ rating-service/src/main/java/com/rahul/rating_service/entity/Rating.java
@@ -17,6 +17,6 @@ public class Rating {
     private String ratingId;
     private String userId;
     private String hotelId;
-    private  int rating;
-    private  String feedback;
+    private int rating;
+    private String feedback;
 }
