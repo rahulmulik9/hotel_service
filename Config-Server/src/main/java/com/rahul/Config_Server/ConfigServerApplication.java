5cf121add897f3bf3c8e46f2e8982061218bdb3d
RAHUL <rahulmulik9@gmail.com>
2025-01-28 22:17:50 +0530

    Initialise the config server

--- Config-Server/src/main/java/com/rahul/Config_Server/ConfigServerApplication.java
+++ Config-Server/src/main/java/com/rahul/Config_Server/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.rahul.ConfigServer;
+package com.rahul.Config_Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Config-Server/src/test/java/com/rahul/Config_Server/ConfigServerApplicationTests.java
+++ Config-Server/src/test/java/com/rahul/Config_Server/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rahul.ConfigServer;
+package com.rahul.Config_Server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
